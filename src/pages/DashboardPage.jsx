586330c54102ef1190da08255913754b6e3ae075
Karl <tekamkarl@gmail.com>
2025-01-03 00:08:38 +0100

    Sdebar is now moved from folder Sidebar to StaticComponents

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Sidebar from "../components/Sidebar/Sidebar";
+import Sidebar from "../components/StaticComponents/Sidebar";
 import Dashboard from "../components/Dashboard/Dashboard";
 
 const DashboardPage = () => {
