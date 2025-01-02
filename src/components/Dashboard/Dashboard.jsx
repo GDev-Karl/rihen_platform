010a9d88ca5699de99d837df4d5805e095934b6b
Karl <tekamkarl@gmail.com>
2025-01-02 09:48:28 +0100

    Dashbord component

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,21 +1,32 @@
-import React from "react";
-import Overview from "./Overview";
-import StudyStatistics from "./StudyStatistics";
-import Progress from "./Progress";
-import MyCourses from "./MyCourses";
-import LiveEvents from "./LiveEvents";
-import Activity from "./Activity";
-import "./Dashboard.css";
+import React from 'react';
+import { Box, Grid, Grid2 } from '@mui/material';
+import Overview from './Overview';
+import LiveEvents from './LiveEvents';
+import StudyStatistics from './StudyStatistics';
+import Activity from './Activity';
+import MyCourses from './MyCourses';
 
 const Dashboard = () => {
   return (
-    <main className="dashboard">
-      <h1>Dashboard</h1>
-      <div className="dashboard-content">
-        <Overview />
-        <LiveEvents />
-      </div>
-    </main>
+    <Box sx={{ padding: 3 }}>
+      <Grid2 container spacing={2}>
+        <Grid2 item xs={12}>
+          <Overview />
+        </Grid2>
+        <Grid2 item xs={8}>
+          <StudyStatistics />
+        </Grid2>
+        <Grid2 item xs={4}>
+          <LiveEvents />
+        </Grid2>
+        <Grid2 item xs={8}>
+          <MyCourses />
+        </Grid2>
+        <Grid2 item xs={4}>
+          <Activity />
+        </Grid2>
+      </Grid2>
+    </Box>
   );
 };
 
