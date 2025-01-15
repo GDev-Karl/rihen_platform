3895c2355c3a9e8601b20ee993f26cd568952716
Karl <tekamkarl@gmail.com>
2025-01-15 19:52:23 +0100

    changes Cursus name

--- frontend/src/components/StaticComponents/Average.jsx
+++ frontend/src/components/StaticComponents/Average.jsx
@@ -3,7 +3,7 @@ import { Paper, Typography, Divider } from "@mui/material";
 
 const Average = () => (
   <Paper sx={{ p: 2 } }>
-    <Typography variant="h6">Scores - Game Development</Typography>
+    <Typography variant="h6">Game Development</Typography>
     <Divider sx={{ my: 1 }} />
     <Typography>Average: 68.95%</Typography>
   </Paper>
