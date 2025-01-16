ee620701177ee235b0bd062778099f77f3f740dc
Karl <tekamkarl@gmail.com>
2025-01-16 07:45:36 +0100

    addde others routes to the register routes

--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -1,11 +1,20 @@
 from flask import Flask
 from app.extensions import api
-from app.routes.user import user_ns
+from app.routes.courses import course_ns
+from app.routes.curriculum import curriculum_ns
+from app.routes.evaluation import evaluation_ns
+from app.routes.module import module_ns
 from app.routes.overview import overview_ns
+from app.routes.projets import project_ns
+from app.routes.task import task_ns
+from app.routes.user import user_ns
 
 def register_routes(app):
-    # Ajoute les namespaces
-    #api.add_namespace(auth_ns, path="/auth")
-    #api.add_namespace(course_ns, path="/courses")
-    api.add_namespace(user_ns, path="/users")
+    api.add_namespace(course_ns, path="/courses")
+    api.add_namespace(curriculum_ns, path="/curriculum")
+    api.add_namespace(evaluation_ns, path="/evaluation")
+    api.add_namespace(module_ns, path="/modules")
     api.add_namespace(overview_ns, path="/overview")
+    api.add_namespace(project_ns, path="/projects")
+    api.add_namespace(task_ns, path="/tasks")
+    api.add_namespace(user_ns, path="/users")
