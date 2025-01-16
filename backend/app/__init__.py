3ae456b8b48898dd3b39f78a47172c860063249b
Karl <tekamkarl@gmail.com>
2025-01-16 07:44:47 +0100

    change comment language

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,18 +1,20 @@
 from flask import Flask
 from app.extensions import db, migrate, jwt, api
 from app.routes import register_routes
+from flask_cors import CORS
 
 def create_app(config_class="config.Config"):
     app = Flask(__name__)
     app.config.from_object(config_class)
+    CORS(app, origins=["http://localhost:3000"]) #frontend url
 
-    # Initialiser les extensions
+    # Initialize extensions
     db.init_app(app)
     api.init_app(app)
     migrate.init_app(app, db)
     #jwt.init_app(app)
 
-    # Enregistrer les routes
+    # Save the routes
     register_routes(app)
 
     return app
