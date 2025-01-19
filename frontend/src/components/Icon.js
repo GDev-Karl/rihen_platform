15714e1acc3d62719cd2d81537d22891b79b1def
Karl <tekamkarl@gmail.com>
2025-01-19 18:21:45 +0100

    comment xmls link

--- frontend/src/components/Icon.js
+++ frontend/src/components/Icon.js
@@ -78,7 +78,7 @@ export const CircularText = ({ className, ...rest }) => (
   <svg
     version="1.1"
     id="Layer_1"
-    xmlns="http://www.w3.org/2000/svg"
+    //xmlns="http://www.w3.org/2000/svg"
     width="496"
     height="496"
     viewBox="0 0 1024 1024"
