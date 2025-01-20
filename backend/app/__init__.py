3350f84470c5b8ef0d2420fe10285a687ef31c2e
Karl <tekamkarl@gmail.com>
2025-01-20 17:40:04 +0100

    added swagger

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from app.extensions import db, migrate, jwt, api
 from app.routes import register_routes
 from flask_cors import CORS
@@ -6,17 +6,24 @@ from flask_cors import CORS
 def create_app(config_class="config.Config"):
     app = Flask(__name__)
     app.config.from_object(config_class)
-    CORS(app, origins=["http://localhost:3000"]) #frontend url
+    app.config['RESTX_VALIDATE'] = True  # Validation automatique des schémas RESTx
+    app.config['REST_MASK_SWAGGER'] = False  # Éviter les masquages dans Swagger UI
 
-    # Initialize extensions
+    # Initialiser les extensions
     db.init_app(app)
     api.init_app(app)
     migrate.init_app(app, db)
-    #jwt.init_app(app)
+    jwt.init_app(app)
 
-    # Save the routes
+    # Configurer CORS
+    CORS(app)
+
+    # Configurer les fichiers statiques pour Swagger UI
+    @app.route('/swaggerui/<path:filename>')
+    def swagger_static(filename):
+        return send_from_directory('static/swaggerui', filename)
+
+    # Enregistrer les routes
     register_routes(app)
 
     return app
-
-
--- backend/config.py
+++ backend/config.py
@@ -3,12 +3,4 @@ import os
 class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql://karl:adminkarl@localhost/rihen_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    REST_MASK_SWAGGER = False
-    
-"""
-    class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "rihen_universe_secret_key")
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI", "sqlite:///rihen_universe.db")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "rihen_universe_jwt_secret_key")
-"""
+    #SECRET_KEY = os.environ.get('SECRET_KEY', 'dev_secret_key')  # Clé secrète pour JWT
